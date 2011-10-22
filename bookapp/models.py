9cfeee543e4ac0299d32e382c35ae245b3dae404
chronotrope <ruben.go.robles@gmail.com>
2011-10-22 11:28:24 -0700

    added code for BookPostAdmin

--- bookapp/models.py
+++ bookapp/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
-class BlogPost(models.Model):
+class BookPost(models.Model):
     book_title = models.CharField(max_length=150)
     book_author = models.CharField(max_length=150)
     book_desc = models.TextField()
-    isbn = models.CharField(max_length=150)
+    isbn = models.CharField(max_length=150, blank=True)
     rating = models.IntegerField()
     image_url = models.CharField(max_length=150)
-
-
+    timestamp = models.DateTimeField()
+admin.site.register(BookPost)
+    
+class BookPostAdmin(admin.ModelAdmin):
+    list_dispay = ('book_title', 'timestamp')
